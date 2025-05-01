4b6ee129459b1e23b36c6e1984a734ebd83e0fec
Sanjana Deshan <sdeshan960@gmail.com>
2025-05-02 00:18:48 +0530

    Create NotificationRepository.java
    fix error in notification repository

---
+++ src/main/java/backend/repository/NotificationRepository.java
@@ -0,0 +1,7 @@
+package backend.repository;
+
+import backend.model.NotificationModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
