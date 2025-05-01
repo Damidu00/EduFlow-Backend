9419c01d09db5230700bf7c4f954746aaa8bbd0d
Sanjana Deshan <sdeshan960@gmail.com>
2025-05-02 00:22:31 +0530

    Update NotificationRepository.java
    fix notification repository

--- src/main/java/backend/repository/NotificationRepository.java
+++ src/main/java/backend/repository/NotificationRepository.java
@@ -5,3 +5,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+@Repository
+public interface NotificationRepository extends MongoRepository<NotificationModel, String> {
+    List<NotificationModel> findByUserId(String userId);
+}
